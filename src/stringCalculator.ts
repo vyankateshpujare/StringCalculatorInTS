660740859c5e6e290c42639b3194d3552d06b4d5
vyankatesh <vyankatesh@aspenify.com>
2024-10-19 13:46:16 +0530

    implemented add function

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,35 @@
-export function add(input: string): number{
+export function add(numbers: string): number {
+    try {
+        if (numbers === "")
+            return 0;
 
-    return 1
+        let delimiter = ",";
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n");
+            delimiter = parts[0].substring(2);
+            numbers = parts[1];
+        }
 
+        const stringArray = numbers.split(new RegExp(`[${delimiter}\n]`));
+
+        let negativesNumArray: number[] = [];
+        let sum: number = 0;
+        stringArray.forEach(element => {
+            let number = Number(element)
+            if (!NaN) {
+                if (number < 0) {
+                    negativesNumArray.push(number)
+                } else if (number <= 1000) {
+                    sum += number;
+                }
+            }
+        });
+
+        if (negativesNumArray.length) {
+            throw new Error("Negative numbers not allowed: " + negativesNumArray.join(","));
+        }
+        return sum
+    } catch (error) {
+        throw error
+    }
 }
